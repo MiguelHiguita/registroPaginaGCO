fd886e56000e0d84ff6d13b699fd30429b0b95e2
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-12 14:49:46 -0500

    crear registro y login de la pagina

---
+++ front/src/components/layouts/auth/Login.jsx
@@ -0,0 +1,104 @@
+import userLogo from "../../../assets/logogco.jpg";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import './Login.css'
+import axios from "axios";
+let apiUsuarios = "http://localhost:5174/usuarios";
+
+const Login = () => {
+  const [getUsuario, setUsuario] = useState("");
+  const [getContrasena, setContrasena] = useState("");
+  const [getCorreo, setCorreo] = useState("");
+  let redireccion = useNavigate()
+
+  const [usuarios, setUsuarios] = useState([]);
+  const getUsuarios = async () => {
+    let resultado = await axios.get(apiUsuarios);
+    console.log(usuarios)
+    setUsuarios(resultado.data);
+  };
+
+  useEffect(() => {
+    getUsuarios()
+  }, [])
+
+  const buscarUsuario = () => {
+    let estado = usuarios.some((usuario) => {
+      if (
+        getUsuario === usuario.usuario &&
+        getContrasena === usuario.contrasena &&
+        getCorreo === usuario.correo
+      ) {
+        return true;
+      }
+    });
+    return estado;
+  };
+  const iniciarSesion = () => {
+    if (buscarUsuario()) {
+      Swal.fire({
+        title: "Bienvenido",
+        text: "Será redireccionado a la página principal",
+        icon: "success",
+      });
+      redireccion('/home')
+    } else {
+      Swal.fire({
+        title: "Error de credenciales",
+        text: "Usuario y/o contraseña no existe o errados",
+        icon: "error",
+      });
+    }
+  };
+
+  const registrarUsuario = () => {
+    redireccion('/registrar-usuario')
+  }
+
+  return (
+    <form className="login-form">
+      <img src={userLogo} alt="Usuario" />
+      <h2>Iniciar sesión</h2>
+      <div className="input-group">
+        <label htmlFor="username">Usuario:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setUsuario(e.target.value);
+          }}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="password">Contraseña:</label>
+        <input
+          onChange={(e) => {
+            setContrasena(e.target.value);
+          }}
+          type="text"
+          name=""
+          id=""
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="email">Correo electrónico:</label>
+        <input
+          onChange={(e) => {
+            setCorreo(e.target.value);
+          }}
+          type="text"
+          name=""
+          id=""
+        />
+      </div>
+      <button onClick={iniciarSesion} type="button">
+        Iniciar sesión
+      </button>
+      <button onClick={registrarUsuario} type="button">
+        Registrar usuario
+      </button>
+    </form>
+  );
+};
+
+export default Login;
---
+++ front/src/components/layouts/pages/usuarios/ListarUsuarios.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+let apiUsuarios = "http://localhost:5174/usuarios";
+
+const ListarUsuarios = () => {
+  const [usuarios, setUsuarios] = useState([]);
+  const getUsuarios = async () => {
+    let resultado = await axios.get(apiUsuarios);
+    console.log(usuarios)
+    setUsuarios(resultado.data);
+  };
+
+  useEffect(() => {
+    getUsuarios()
+  }, [])
+
+  console.log(usuarios);
+  return (
+    <div className="cards">
+      {usuarios.map((usuario) => (
+        <section>
+          <p>Usuario: {usuario.usuario}</p>
+          <p>Contraseña: {usuario.contrasena}</p>
+          <p>Correo:{usuario.correo}</p>
+          <div>
+            <button>Eliminar</button>
+            <button>Editar</button>
+          </div>
+        </section>
+      ))}
+    </div>
+  );
+};
+
+export default ListarUsuarios;
